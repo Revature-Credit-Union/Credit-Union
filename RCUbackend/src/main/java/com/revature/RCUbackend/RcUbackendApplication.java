89f4907450090af4e1a0ce0b0d2a221d7725fc8c
Hacker Man <hackerman38294@gmail.com>
2021-12-02 10:53:07 -0600

    project setup

---
+++ RCUbackend/src/main/java/com/revature/RCUbackend/RcUbackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RCUbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcUbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcUbackendApplication.class, args);
+	}
+
+}
