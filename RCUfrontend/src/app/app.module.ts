5c16eb3b51a3ade959b94ee062a86a3d873e57f8
Alex <alexeasley40@yahoo.com>
2021-12-06 16:36:23 -0500

    Adding Account Settings Component

--- RCUfrontend/src/app/app.module.ts
+++ RCUfrontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountSettingsComponent
   ],
   imports: [
     BrowserModule,
