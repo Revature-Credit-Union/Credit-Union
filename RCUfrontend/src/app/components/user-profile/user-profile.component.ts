bc570dfd73ab2ab746ff079e6ab2d31a2edb8162
Jorge Tinajero <jotinajero07@hotmail.com>
2021-12-07 16:25:11 -0600

    added popup

--- RCUfrontend/src/app/components/user-profile/user-profile.component.ts
+++ RCUfrontend/src/app/components/user-profile/user-profile.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
+
+
+
 export class UserProfileComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    document.getElementById("edit-Info")!.style.display = "none";
+  }
+
+  
+  edit(){
+    document.getElementById("edit-Info")!.style.display = "block";
+    document.getElementById("myInfo")!.style.filter = "blur(4px)"; //blur will blur the background content to make it look nicer
+  }
+ 
+  close(){
+    document.getElementById("edit-Info")!.style.display = "close";
   }
 
 }
