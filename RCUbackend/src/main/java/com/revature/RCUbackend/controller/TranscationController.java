92c441a49dacdbe7090bea4865146a0d01b2b418
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-12-07 07:38:14 -0800

    Modifying this file to avoid merge conflicts

--- RCUbackend/src/main/java/com/revature/RCUbackend/controller/TranscationController.java
+++ RCUbackend/src/main/java/com/revature/RCUbackend/controller/TranscationController.java
@@ -1,5 +1,6 @@
 package com.revature.RCUbackend.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.RCUbackend.models.Response;
 import com.revature.RCUbackend.models.Transaction;
+import com.revature.RCUbackend.models.User;
 import com.revature.RCUbackend.service.TransactionService;
 
 @RestController("transactionController")
@@ -27,6 +30,20 @@ public class TranscationController {
 	}
 	
 	//any of these mappings are subject to change depending on HTTP requests from frontend
+	
+    @GetMapping(path = "/allTransactions", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response allTransactions(@RequestBody User user){
+        Response response;
+        ArrayList<Transaction> temp = (ArrayList<Transaction>) this.transactionService.findByUserId(user.getUserID());
+
+        if(temp != null){
+            response = new Response(true, "Transactions found", temp);
+        }else{
+            response = new Response(false, "Transactions not found", null);
+        }
+
+        return response;
+    }
 	
 	@PostMapping(path = "/new", consumes = MediaType.APPLICATION_JSON_VALUE) 
 	public Transaction saveTransaction(@RequestBody Transaction transaction) {
