d3ecb644bbf560f2a788f3eeea82847555df8c89
Jameson Bryant <78752988+DebugWhiskey@users.noreply.github.com>
2021-12-09 13:25:32 -0700

    Styling and prep work
    Changed the look of User-profile and added Change Password button.

--- RCUfrontend/src/app/components/user-profile/user-profile.component.ts
+++ RCUfrontend/src/app/components/user-profile/user-profile.component.ts
@@ -10,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class UserProfileComponent implements OnInit {
 
+  // Placeholders for myInfo variables, to be swapped out for database references
+  firstName: string = "Charles";
+  lastName: string = "Dickens";
+  email: string = "dude@myguy.org";
+
   constructor() { }
 
   ngOnInit(): void {
     document.getElementById("edit-Info")!.style.display = "none";
+    document.getElementById("edit-Pass")!.style.display = "none";
   }
 
   
@@ -26,6 +32,12 @@ export class UserProfileComponent implements OnInit {
  
   close(){
     document.getElementById("edit-Info")!.style.display = "close";
+  }
+
+  // Edit Password Method
+  changePass() {
+    document.getElementById("edit-Pass")!.style.display = "block";
+    document.getElementById("myInfo")!.style.filter = "blur(4px)"; //blur will blur the background content to make it look noicer
   }
 
 }
