1d31827cb35568fe12cf3564967355e762a9c310
yousefkami <49797583+yousefkami@users.noreply.github.com>
2021-12-10 18:33:10 -0500

    merged conflicts delt with PART 1

--- RCUbackend/src/main/java/com/revature/RCUbackend/repositories/AccountRepository.java
+++ RCUbackend/src/main/java/com/revature/RCUbackend/repositories/AccountRepository.java
@@ -22,26 +22,3 @@ public interface AccountRepository extends JpaRepository<Account, Integer>{
 	public void withdrawAccount(int amount, int accountID);
 }
 
-package com.revature.RCUbackend.repositories;
-
-import com.revature.RCUbackend.models.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Integer>{
-	public List<Account> findAll();
-	
-	@Modifying
-	@Query("update Account a SET a.balance = a.balance + ?1 WHERE a.accountId = ?2")
-	public void depositAccount(int amount, int accountID);
-	
-	@Modifying
-	@Query("update Account a SET a.balance = a.balance - ?1 WHERE a.accountId = ?2")
-	public void withdrawAccount(int amount, int accountID);
-}
-
