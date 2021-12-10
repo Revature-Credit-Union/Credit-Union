ea009ab13595bd73ec02f368431960d4d9e34d51
Jorge Tinajero <jotinajero07@hotmail.com>
2021-12-10 14:13:40 -0600

    fixed error with pass div

--- RCUfrontend/src/app/components/user-profile/user-profile.component.ts
+++ RCUfrontend/src/app/components/user-profile/user-profile.component.ts
@@ -19,7 +19,6 @@ export class UserProfileComponent implements OnInit {
 
   ngOnInit(): void {
     document.getElementById("edit-Info")!.style.display = "none";
-    document.getElementById("edit-Pass")!.style.display = "none";
   }
 
   
@@ -30,15 +29,8 @@ export class UserProfileComponent implements OnInit {
  
   close(){
     document.getElementById("edit-Info")!.style.display = "none";
-    document.getElementById("edit-Pass")!.style.display = "none";
     document.getElementById("myInfo")!.style.filter = "blur(0px)"; //blur will blur the background content to make it look nicer
 
-  }
-
-  // Edit Password Method
-  changePass() {
-    document.getElementById("edit-Pass")!.style.display = "block";
-    document.getElementById("myInfo")!.style.filter = "blur(4px)"; //blur will blur the background content to make it look noicer
   }
 
 }
