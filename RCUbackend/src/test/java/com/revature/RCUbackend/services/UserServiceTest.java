d911944145d9a115496f136a0dccc3f7cf08905e
TMSAVANG <thanath.savang@revature.net>
2021-12-15 09:42:04 -0500

    Revert "Merge branch 'TeamOne' into footer"
    This reverts commit bfae5d4ac80b3140b0703eb7adae0bd170987b62, reversing
    changes made to d22e9221d61181e9538885fc4c56d2774153dede.

--- RCUbackend/src/test/java/com/revature/RCUbackend/services/UserServiceTest.java
+++ RCUbackend/src/test/java/com/revature/RCUbackend/services/UserServiceTest.java
@@ -1,61 +1,22 @@
 package com.revature.RCUbackend.services;
 
-import static org.assertj.core.api.Assertions.*;
 
-import com.revature.RCUbackend.models.User;
 import com.revature.RCUbackend.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 //import org.junit.runner.RunWith;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
-	
     @Autowired
     private UserService userService;
 
     @MockBean
     private UserRepo userRepo;
-    
-    @Test
-    public void testGetAllUsers() {
-    	Mockito.when(userRepo.findAll()).thenReturn(new ArrayList<User>());
-    	List<User> testList = userService.getAllUsers();
-    	assertThat(testList).isNotNull();
-    }
-    
-    @Test
-    public void testGetUser() {
-    	Optional<User> testOptional = Optional.of(new User());
-    	Mockito.when(userRepo.findById(Mockito.anyInt())).thenReturn(testOptional);
-    	User testUser = userService.getUser(Mockito.anyInt());
-    	assertThat(testUser).isNotNull();
-    }
-    
-    @Test
-    public void testFindByEmail() {
-    	Optional<User> testOptional = Optional.of(new User());
-    	Mockito.when(userRepo.findByEmail(Mockito.anyString())).thenReturn(testOptional);
-    	User testUser = userService.findByEmail(Mockito.anyString()).get();
-    	assertThat(testUser).isNotNull();
-    }
-    
-    @Test
-    public void testFindByUsername() {
-    	Optional<User> testOptional = Optional.of(new User());
-    	Mockito.when(userRepo.findByUsername(Mockito.anyString())).thenReturn(testOptional);
-    	User testUser = userService.findByUsername(Mockito.anyString()).get();
-    	assertThat(testUser).isNotNull();
-    }
+
 }
