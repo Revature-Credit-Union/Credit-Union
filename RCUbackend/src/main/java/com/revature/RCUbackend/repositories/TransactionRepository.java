ab9e092e35fc4763939a25f97f9b4a50b0b05855
KCastilloRevature <88732741+KCastilloRevature@users.noreply.github.com>
2021-12-17 12:48:20 -0800

    put up some documentation

--- RCUbackend/src/main/java/com/revature/RCUbackend/repositories/TransactionRepository.java
+++ RCUbackend/src/main/java/com/revature/RCUbackend/repositories/TransactionRepository.java
@@ -1,22 +1,52 @@
 package com.revature.RCUbackend.repositories;
 
-import com.revature.RCUbackend.models.Transaction;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.revature.RCUbackend.models.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer>{
+	/**
+     * This method retrieves all Transactions from the database
+     * This method can be removed in favor of the built-in findAll method
+     * that comes with extending JpaRepository
+     * @return the list of Transaction objects
+	 */
 	public List<Transaction> findAll();
 	
+	/**
+     * This method persists the Transaction to the database
+     * This method can be removed in favor of the built-in save method
+     * that comes with extending JpaRepository
+	 * @param transaction the Transaction object to be saved
+	 * @return the Transaction object that was saved
+	 */
 	public default Transaction saveTransaction(Transaction transaction) {
 		return save(transaction);
 	}
 	
+	/**
+	 * This method finds the Transaction by the desired transactionID
+	 * @param id the int representing the transactionID of the transaction
+	 * @return the Transaction object of the desired transactionID
+	 */
 	public Transaction findByTransactionID(int id);
 
+	/**
+	 * This method finds the Transactions by the desired accountID
+	 * @param id the int representing the accountID of the transaction
+	 * @return the list of Transaction objects of the desired accountID
+	 */
 	public List<Transaction> findByAccountID(int id);
 	
+	/**
+	 * This method finds the Transactions by the desired transaction type and accountID
+	 * @param type the int representing the transaction type of the transaction
+	 * @param id the int representing the accountID of the transaction
+	 * @return the list of Transaction objects of the desired transaction type and accountID
+	 */
 	public List<Transaction> findByTransactionTypeAndAccountID(int type, int id);
 }
